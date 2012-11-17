691969e416b984b83ac48267706c5777c6cd9517
konradd <konraddymek@gmail.com>
2012-11-17 20:40:48 +0100

    22
    example.php w pełni dynamiczne

--- example.php
+++ example.php
@@ -7,19 +7,25 @@
 
   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
   <?php
-
-$link = mysql_connect("mysql3.ph-hos.osemka.pl", "1352208389_f", "1Bond007")
-    or die("Problemy z bazą danych, odśwież stronę kilkukrotnie, powinno pomóc<br /> Admin");
-mysql_query('SET NAMES utf8');
-mysql_select_db("1206275_trails")
-    or die("Could not select database");
-
-$query1  = "SELECT Trasy.Kolor, Kolory.Kolor FROM Trasy, Kolory WHERE Trasy.ID=1 AND Kolory.ID=Trasy.Kolor";
-$result1 = mysql_query($query1)
-    or die("Query1 failed");
-while ($row = mysql_fetch_array($result1)) 
-	$color= $row["Kolor"];	
-	print ("<title>$color</title>");
+//DATABASE
+	$link = mysql_connect("mysql3.ph-hos.osemka.pl", "1352208389_f", "1Bond007")
+	    or die("Problemy z bazą danych, odśwież stronę kilkukrotnie, powinno pomóc<br /> Admin");
+	mysql_query('SET NAMES utf8');
+	mysql_select_db("1206275_trails")
+	    or die("Could not select database");
+//PASEK - KOLOR/SZCZYT
+	$query1  = "SELECT Trasy.Kolor, Kolory.Kolor FROM Trasy, Kolory WHERE Trasy.ID=1 AND Kolory.ID=Trasy.Kolor";
+	$result1 = mysql_query($query1)
+	    or die("Query1 failed");
+	while ($row = mysql_fetch_array($result1)) 
+		$color= $row["Kolor"];	
+		print ("<title>$color");
+	$query0  = "SELECT Trasy.Koniec, Szczyty.Szczyt FROM Trasy, Szczyty WHERE Trasy.ID=1 AND Szczyty.ID=Trasy.Koniec";
+	$result0 = mysql_query($query0)
+	    or die("Query0 failed");
+	while ($row = mysql_fetch_array($result0)) 
+		$end= $row["Szczyt"];	
+		print (" - $end</title>");
 	
 
   
@@ -64,94 +70,136 @@ echo '
         </div>
 
             <div id="right"> ';
-$query2  = "SELECT Pasma.Pasmo, Podpasma.Podpasmo, Podpasma.Adres FROM Podpasma, Pasma WHERE Podpasma.ABC = Pasma.id AND Pasma.id = 1";
-$result2 = mysql_query($query2)
-    or die("Query2 failed");
-    
-print ("<h1>Góry Świętokrzyskie</h1><ul>");    
-while ($row = mysql_fetch_array($result2)) {
-	$ad= $row["Adres"];  	 
-	echo "<li><a href=$ad>" . $row["Podpasmo"]  .
-	     "</a></li>" ;
-}
-print ("</ul>");	 	     
-
-
-
-$query3  = "SELECT Pasma.Pasmo, Podpasma.Podpasmo, Podpasma.Adres FROM Podpasma, Pasma WHERE Podpasma.ABC = Pasma.id AND Pasma.id = 2";
-$result3 = mysql_query($query3)
-    or die("Query3 failed");
-    
-print ("<h1>Karpaty</h1><ul>");    
-while ($row = mysql_fetch_array($result3)) {
-	$ad= $row["Adres"];  	 
-	echo "<li><a href=$ad>" . $row["Podpasmo"]  .
-	     "</a></li>" ;
-}
-print ("</ul>");	
- 	     
-    
-$query4  = "SELECT Pasma.Pasmo, Podpasma.Podpasmo, Podpasma.Adres FROM Podpasma, Pasma WHERE Podpasma.ABC = Pasma.id AND Pasma.id = 3";
-$result4 = mysql_query($query4)
-    or die("Query4 failed");
-    
-print ("<h1>Sudety</h1>");  
-print ("<ul>");
-while ($row = mysql_fetch_array($result4)) {
-	$ad= $row["Adres"];  	 
-	echo "<li><a href=$ad>" . $row["Podpasmo"]  .
-	     "</a></li>" ;
-}	     
-print ("</ul>");
+//PASMA/PODPASMA            
+	$query2  = "SELECT Pasma.Pasmo, Podpasma.Podpasmo, Podpasma.Adres FROM Podpasma, Pasma WHERE Podpasma.ABC = Pasma.id AND Pasma.id = 1";
+	$result2 = mysql_query($query2)
+	    or die("Query2 failed");
+	    
+	print ("<h1>Góry Świętokrzyskie</h1><ul>");    
+	while ($row = mysql_fetch_array($result2)) {
+		$ad= $row["Adres"];  	 
+		echo "<li><a href=$ad>" . $row["Podpasmo"]  .
+		     "</a></li>" ;
+	}
+	print ("</ul>");	 	     
+	
+	
+	
+	$query3  = "SELECT Pasma.Pasmo, Podpasma.Podpasmo, Podpasma.Adres FROM Podpasma, Pasma WHERE Podpasma.ABC = Pasma.id AND Pasma.id = 2";
+	$result3 = mysql_query($query3)
+	    or die("Query3 failed");
+	    
+	print ("<h1>Karpaty</h1><ul>");    
+	while ($row = mysql_fetch_array($result3)) {
+		$ad= $row["Adres"];  	 
+		echo "<li><a href=$ad>" . $row["Podpasmo"]  .
+		     "</a></li>" ;
+	}
+	print ("</ul>");	
+	 	     
+	    
+	$query4  = "SELECT Pasma.Pasmo, Podpasma.Podpasmo, Podpasma.Adres FROM Podpasma, Pasma WHERE Podpasma.ABC = Pasma.id AND Pasma.id = 3";
+	$result4 = mysql_query($query4)
+	    or die("Query4 failed");
+	    
+	print ("<h1>Sudety</h1>");  
+	print ("<ul>");
+	while ($row = mysql_fetch_array($result4)) {
+		$ad= $row["Adres"];  	 
+		echo "<li><a href=$ad>" . $row["Podpasmo"]  .
+		     "</a></li>" ;
+	}	     
+	print ("</ul>");
 
         
                                                     
-echo       ' </div>          
-  
-            <div id="main">
-	<div id="data">
-		 <div id="through">';
-$query5  = "SELECT Trasy.Przez_I, Trasy.Przez_II, Trasy.Przez_III FROM Trasy WHERE Trasy.ID=1";
-$result5 = mysql_query($query5)
-    or die("Query5 failed");
-while ($row = mysql_fetch_array($result5)) 
-	for ($i=1; i>3; $i++)
-		$through=$row[$i];
-		if ($through!="NULL")
-			print ("$through<br />");
-	
-print	 ("</div>");
+echo ' </div>          
 
-$query  = "SELECT Trasy.Kolor, Kolory.Kolor FROM Trasy, Kolory WHERE Trasy.ID=1 AND Kolory.ID=Trasy.Kolor";
-$result = mysql_query($query)
-    or die("Query failed");
-while ($row = mysql_fetch_array($result)) 
-	$color= $row["Kolor"];
-	print ("<h1>$color</h1>");	  			
-echo'
-     <h2>Kozia Góra - Beskid Śląski</h2>
-     6 km <br />
-     100 m  <img src="Grafiki/pion.png"/> <br />
-     
-      <div id="trudn">
-	   <h1>Trudność:</h1>
-       7<sup>/10</sup>
-      </div>
-      
-      <div id="fun">
-       <h1>Fun:</h1>
-       8<sup>/10</sup>
-      </div>
-      
-       
-    
-		<iframe src="http://osm.trail.pl/embed?ll=19.053125381469727%2C+49.779878500562425&amp;l1=Zielony+szlak&amp;l2=Bielsko-Bia%C5%82a+-+Kozia+G%C3%B3ra&amp;l3=Beskid+%C5%9Al%C4%85ski&amp;w=400&amp;h=400&amp;z=14&amp;m=1" frameborder="0" width="400" height="400"></iframe>
-		</div>
-			<div id="opis">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac tortor risus. Nam pulvinar, dolor non posuere tincidunt, augue tellus facilisis justo, quis posuere nulla orci quis elit. Nam vestibulum, lectus et scelerisque commodo, lacus neque convallis nibh, nec lobortis ligula odio at tortor. Morbi et dignissim metus. Fusce nec metus elit, at vehicula nisl. Donec nec accumsan risus. Mauris egestas fringilla orci vitae varius. Mauris ultrices mattis ultrices. Proin dapibus dolor eu tortor tristique ut porttitor eros pellentesque. Aenean leo urna, mollis vel consectetur sit amet, venenatis id sapien. Vivamus auctor facilisis orci nec molestie. Suspendisse potenti.
-
-Etiam commodo quam non ligula pharetra pellentesque. Quisque et felis id nunc rhoncus ornare et a tortor. Curabitur augue nisl, mattis fermentum ullamcorper vel, porta ultricies tortor. Sed et egestas lectus. Suspendisse non est lacinia felis accumsan laoreet. Ut tortor diam, tristique eget venenatis eget, vulputate suscipit metus. Mauris interdum justo quis velit adipiscing sit amet vehicula justo suscipit.</div>
+	<div id="main">
+		<div id="data">
+			<div id="through">';
+//PRZEZ
+	$query5  = "SELECT Trasy.Przez_I, Trasy.Przez_II, Trasy.Przez_III FROM Trasy WHERE Trasy.ID=1";
+	$result5 = mysql_query($query5)
+		or die("Query5 failed");
+	while ($row = mysql_fetch_array($result5)) 
+		for ($i=1; i>3; $i++)
+			$through=$row[$i];
+			if ($through!="NULL")
+				print ("$through<br />");
 		
-	        </div>
+	print	 ("</div>");
+	
+//KOLOR
+	global $color;
+	print ("<h1>$color</h1>");
+	
+	  			
+// SZCZYT - PASMO
+     global $end;
+     print ("<h2> $end ");
+     $query6  = "SELECT Trasy.Pasmo, Podpasma.Podpasmo FROM Trasy, Podpasma WHERE Trasy.ID=1 AND Podpasma.ID=Trasy.Pasmo";
+	 $result6 = mysql_query($query6)
+		 or die("Query6 failed");
+	 while ($row = mysql_fetch_array($result6)) 
+	 $range= $row["Podpasmo"];	
+	 print (" - $range</h2>");
+
+     
+ 
+// DŁUGOŚĆ - PRZEWYŻSZENIE
+	 $query7  = "SELECT Trasy.Dlugosc FROM Trasy WHERE Trasy.ID=1 ";
+	 $result7 = mysql_query($query7)
+		 or die("Query7 failed");
+	 while ($row = mysql_fetch_array($result7)) 
+	 $lenght= $row["Dlugosc"];
+	 	 print ("$lenght km <br />");
+	 $query8  = "SELECT Trasy.Pion FROM Trasy WHERE Trasy.ID=1 ";
+	 $result8 = mysql_query($query8)
+		 or die("Query8 failed");
+	 while ($row = mysql_fetch_array($result8)) 
+	 $vert= $row["Pion"];
+	 print ("$vert m  ");
+	 print ('<img src="Grafiki/pion.png"/> <br />');
+	 
+echo'    
+      <div id="trudn">
+	   <h1>Trudność:</h1>';
+	   
+//TRUDNOSC - FUN
+	 $query9  = "SELECT Trasy.Trudnosc FROM Trasy WHERE Trasy.ID=1 ";
+	 $result9 = mysql_query($query9)
+		 or die("Query9 failed");
+	 while ($row = mysql_fetch_array($result9)) 
+	 $lvl= $row["Trudnosc"];
+	 	 print ("$lvl<sup>/10</sup> </div>");
+	 $query10  = "SELECT Trasy.Fun FROM Trasy WHERE Trasy.ID=1 ";
+	 $result10 = mysql_query($query10)
+		 or die("Query10 failed");
+	 while ($row = mysql_fetch_array($result10)) 
+	 $funn= $row["Fun"];
+	 print ('<div id="fun"> <h1>Fun:</h1>');
+	 print ("$funn<sup>/10</sup></div>");    
+ 
+           
+//MAPA    
+	 $query11  = "SELECT Trasy.Mapa FROM Trasy WHERE Trasy.ID=1 ";
+	 $result11 = mysql_query($query11)
+		 or die("Query11 failed");
+	 while ($row = mysql_fetch_array($result11)) 
+	 $map= $row["Mapa"];
+	 	 print ("$map</div>");
+
+//OPIS
+	 $query12  = "SELECT Trasy.Opis FROM Trasy WHERE Trasy.ID=1 ";
+	 $result12 = mysql_query($query12)
+		 or die("Query12 failed");
+	 while ($row = mysql_fetch_array($result12)) 
+	 $txt= $row["Opis"];
+	 	 print ('<div id="opis">');	
+	 	 print ("$txt </div>");
+ 
+echo'			
 	 
 	
    </div>
